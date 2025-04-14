0e781b23a9ee153569c39da571125ed4969cc062
LIBYONE <p2320647@mpu.edu.mo>
2025-04-14 11:51:06 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,21 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: true,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-          chart: ['chart.js', 'react-chartjs-2'],
-          leaflet: ['leaflet']
-        }
-      }
-    }
-  },
-  base: '/weather-forcast/',
   server: {
     port: 5173,
     host: true
